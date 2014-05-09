6c54c356338b3a4ffad114bbf73338c8fef177eb
maxpowa <maxpowa1@gmail.com>
2014-05-09 03:34:38 -0600

    Reup site

---
+++ Rakefile.rb
@@ -0,0 +1,141 @@
+require "rubygems"
+require "bundler/setup"
+require "stringex"
+
+## -- Config -- ##
+
+public_dir      = "public"    # compiled site directory
+posts_dir       = "_posts"    # directory for blog files
+new_post_ext    = "md"  # default new post file extension when using the new_post task
+new_page_ext    = "md"  # default new page file extension when using the new_page task
+
+desc "Preview _site/"
+task :preview do
+  puts "\n## Starting Jekyll local server"
+  spawn("jekyll serve")
+  puts "\n## Opening _site/ in browser"
+  status = system("open http://0.0.0.0:4000/")
+  puts status ? "Success" : "Failed"
+end
+
+desc "Build _site/ for production"
+task :pro do
+puts "\n## Building Jekyll to _site/"
+status = system("jekyll build")
+puts status ? "Success" : "Failed"
+end
+
+desc "Commit _site/"
+task :commit do
+  puts "\n## Staging modified files"
+  status = system("git add -A")
+  puts status ? "Success" : "Failed"
+  puts "\n## Committing a site build at #{Time.now.utc}"
+  message = "Build site at #{Time.now.utc}"
+  status = system("git commit -m \"#{message}\"")
+  puts status ? "Success" : "Failed"
+  puts "\n## Pushing commits to remote"
+  status = system("git push origin source")
+  puts status ? "Success" : "Failed"
+end
+
+desc "Deploy _site/ to master branch"
+task :deploy do
+  puts "\n## Deleting master branch"
+  status = system("git branch -D master")
+  puts status ? "Success" : "Failed"
+  puts "\n## Creating new master branch and switching to it"
+  status = system("git checkout -b master")
+  puts status ? "Success" : "Failed"
+  puts "\n## Forcing the _site subdirectory to be project root"
+  status = system("git filter-branch --subdirectory-filter _site/ -f")
+  puts status ? "Success" : "Failed"
+  puts "\n## Switching back to source branch"
+  status = system("git checkout source")
+  puts status ? "Success" : "Failed"
+  puts "\n## Pushing all branches to origin"
+  status = system("git push --all origin")
+  puts status ? "Success" : "Failed"
+end
+
+desc "Commit and deploy _site/"
+task :commit_deploy => [:commit, :deploy] do
+end
+
+#############################
+# Create a new Post or Page #
+#############################
+
+# usage rake new_post
+desc "Create a new post in #{posts_dir}"
+task :new_post, :title do |t, args|
+  if args.title
+    title = args.title
+  else
+    title = get_stdin("Enter a title for your post: ")
+  end
+  filename = "#{posts_dir}/#{Time.now.strftime('%Y-%m-%d')}-#{title.to_url}.#{new_post_ext}"
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  tags = get_stdin("Enter tags to classify your post (comma separated): ")
+  puts "Creating new post: #{filename}"
+  open(filename, 'w') do |post|
+    post.puts "---"
+    post.puts "layout: post"
+    post.puts "title: \"#{title.gsub(/&/,'&amp;')}\""
+    post.puts "modified: #{Time.now.strftime('%Y-%m-%d %H:%M:%S %z')}"
+    post.puts "tags: [#{tags}]"
+    post.puts "image:"
+    post.puts "  feature: "
+    post.puts "  credit: "
+    post.puts "  creditlink: "
+    post.puts "comments: "
+    post.puts "share: "
+    post.puts "---"
+  end
+end
+
+# usage rake new_page
+desc "Create a new page"
+task :new_page, :title do |t, args|
+  if args.title
+    title = args.title
+  else
+    title = get_stdin("Enter a title for your page: ")
+  end
+  filename = "#{title.to_url}.#{new_page_ext}"
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  tags = get_stdin("Enter tags to classify your page (comma separated): ")
+  puts "Creating new page: #{filename}"
+  open(filename, 'w') do |page|
+    page.puts "---"
+    page.puts "layout: page"
+    page.puts "permalink: /#{title.to_url}/"
+    page.puts "title: \"#{title}\""
+    page.puts "modified: #{Time.now.strftime('%Y-%m-%d %H:%M')}"
+    page.puts "tags: [#{tags}]"
+    page.puts "image:"
+    page.puts "  feature: "
+    page.puts "  credit: "
+    page.puts "  creditlink: "
+    page.puts "share: "
+    page.puts "---"
+  end
+end
+
+def get_stdin(message)
+  print message
+  STDIN.gets.chomp
+end
+
+def ask(message, valid_options)
+  if valid_options
+    answer = get_stdin("#{message} #{valid_options.to_s.gsub(/"/, '').gsub(/, /,'/')} ") while !valid_options.include?(answer)
+  else
+    answer = get_stdin(message)
+  end
+  answer
+end
---
+++ edit.md
@@ -0,0 +1,12 @@
+---
+layout: page
+permalink: /edit/
+title: WYSIWYG Markdown editor
+tags: [markdown, md, wysiwyg, editor, wysiwyg editor]
+image:
+  feature: dillinger.png
+share: true
+link: http://dillinger.io/
+---
+
+The most useful, and most extensive live markdown editor I've seen yet :)
