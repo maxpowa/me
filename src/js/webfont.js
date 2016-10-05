ec6b1303ffecc319864b374a1cb383f93bfde993
Max Gurela <maxpowa1@gmail.com>
2016-10-05 19:23:28 +0530

    Defer webfont loading & tweak avatar bg

---
+++ src/js/webfont.js
@@ -0,0 +1,11 @@
+ WebFontConfig = {
+   google: {
+     families: ['Raleway:300,400,500']
+   }
+ };
+
+ (function(d) {
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+    s.parentNode.insertBefore(wf, s);
+ })(document);
