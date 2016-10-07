147c9c9cf781e78382847708ad5a4827e1da9169
Max Gurela <maxpowa1@gmail.com>
2016-10-07 11:21:21 +0530

    Update 2016-10-07T05:51:16.885Z

--- js/webfont.js
+++ js/webfont.js
@@ -1,1 +1,1 @@
-WebFontConfig={google:{families:["Raleway:300,400,500"]}},function(e){var o=e.createElement("script"),t=e.scripts[0];o.src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js",t.parentNode.insertBefore(o,t)}(document);
+WebFontConfig={google:{families:["Raleway:400,500"]}},function(e){var o=e.createElement("script"),t=e.scripts[0];o.src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js",o.async=!0,o.defer=!0,t.parentNode.insertBefore(o,t)}(document);
